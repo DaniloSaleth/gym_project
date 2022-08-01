a8163a8df19569d7f2180d17033cbcbc62f107ae
DANILO AUGUSTO SALETH SANTOS <CIANDT\dsaleth@lnb035774bhz.ciandt.global>
2022-08-01 14:26:45 -0300

    Definindo estrutura de pastas e arquitetura

---
+++ app/src/main/java/com/example/gymproject/ui/home/HomeActivity.kt
@@ -0,0 +1,13 @@
+package com.example.gymproject.ui.home
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.gymproject.R
+import com.google.firebase.firestore.FirebaseFirestore
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
